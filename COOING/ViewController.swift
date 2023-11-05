e619d74f60ce64a3c6f5deec670c5e546f2e8d73
binisnil <dbsqlssss0704@gmail.com>
2023-11-05 18:44:55 +0900

    [Setting] #1 프로젝트 생성 및 코드베이스 세팅 완료

---
+++ COOING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COOING
+//
+//  Created by 박윤빈 on 2023/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
