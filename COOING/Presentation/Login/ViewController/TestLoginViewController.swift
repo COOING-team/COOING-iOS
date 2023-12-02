0ed80fceb6a2285cfec6aff0f75f8d69b528b724
CJiu01 <jiwoo020801@gmail.com>
2023-12-02 19:01:58 +0900

    feat/ kakaoLogin test를 위한 testVC 구현 #9

--- COOING/Presentation/Login/ViewController/TestLoginViewController.swift
+++ COOING/Presentation/Login/ViewController/TestLoginViewController.swift
@@ -5,4 +5,41 @@
 //  Created by 최지우 on 12/2/23.
 //
 
-import Foundation
+import UIKit
+import KakaoSDKAuth
+import KakaoSDKUser
+
+class TestLoginViewController: BaseViewController {
+    
+    // MARK: - Properties
+
+        
+    // MARK: - UI Components
+    
+    let kakaoLoginButton = UIButton()
+
+    // MARK: - override Method
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func configureUI() {
+        kakaoLoginButton.do {
+            $0.setImage(Image.kakaoLoginButton, for: .normal)
+        }
+    }
+
+    override func setLayout() {
+        
+        kakaoLoginButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    override func hieararchy() {
+        view.addSubViews(kakaoLoginButton)
+    }
+
+}
