4c96d76943b005a9163623640d6e24bf054ad6b2
CJiu01 <jiwoo020801@gmail.com>
2023-12-04 08:58:44 +0900

    feat/ HomeRouter #32

--- COOING/Network/Router/HomeRouter.swift
+++ COOING/Network/Router/HomeRouter.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+import Moya
+
+enum HomeRouter {
+    case getInfo
+}
+
+extension HomeRouter: TargetType {
+    public var baseURL: URL {
+        return URL(string: Config.baseURL)!
+    }
+
+    var path: String {
+        switch self {
+        case .getInfo:
+            return "/api/v1/home/info"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .getInfo:
+            return .get
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .getInfo:
+            return .requestPlain
+        }
+    }
+
+    var headers: [String: String]? {
+        return ["Content-type": "application/json"]
+    }
+
+}
