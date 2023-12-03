b75393ebf1f89daaaf3a301fcab3d01be9822f83
CJiu01 <jiwoo020801@gmail.com>
2023-12-03 20:26:22 +0900

    feat/ IdentityTokenDTO #15

---
+++ COOING/Network/DTO/Auth/IdentityTokenDTO.swift
@@ -0,0 +1,14 @@
+//
+//  IdentityTokenDTO.swift
+//  COOING
+//
+//  Created by 최지우 on 12/3/23.
+//
+
+import Foundation
+
+
+struct IdentityTokenDTO: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
