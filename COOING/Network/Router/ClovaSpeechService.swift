2976463a4808ded8465d1754337f96df8320d080
CJiu01 <jiwoo020801@gmail.com>
2023-12-04 04:21:49 +0900

    feat/ ClovaSpeechService #24

--- COOING/Network/Router/ClovaSpeechService.swift
+++ COOING/Network/Router/ClovaSpeechService.swift
@@ -4,5 +4,51 @@
 //
 //  Created by 최지우 on 12/4/23.
 //
-
 import Foundation
+
+import AVFoundation
+import Moya
+
+enum ClovaSpeechService {
+    case recognizeAudio(audioData: Data, language: String)
+}
+
+extension ClovaSpeechService: TargetType {
+    var baseURL: URL {
+        return URL(string: "https://clovaspeech-gw.ncloud.com")!
+    }
+    
+    var path: String {
+        switch self {
+        case .recognizeAudio:
+            return "/recog/v1/stt"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .post
+    }
+    
+    var task: Task {
+        switch self {
+        case .recognizeAudio(let audioData, let language):
+            // Query parameters
+            var params: [String: Any] = [:]
+            params["lang"] = language
+            
+            // Custom encoding to add query parameters to the URL
+            return .requestCompositeData(bodyData: audioData, urlParameters: params)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return [
+            "X-CLOVASPEECH-API-KEY": "f050b703c495437f9d8cb8cd178d3cf5",
+            "Content-Type": "application/octet-stream"
+        ]
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+}
