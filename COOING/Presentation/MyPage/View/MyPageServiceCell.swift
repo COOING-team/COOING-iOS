c30cbca54cb224c14445cb4f89151ec44c7560a3
CJiu01 <jiwoo020801@gmail.com>
2023-12-11 04:51:57 +0900

    feat/ MyPageServiceCell #39

--- COOING/Presentation/MyPage/View/MyPageServiceCell.swift
+++ COOING/Presentation/MyPage/View/MyPageServiceCell.swift
@@ -5,4 +5,60 @@
 //  Created by 최지우 on 12/10/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+class MyPageServiceCell: UITableViewCell {
+    
+    // MARK: = Properties
+    
+    static let identifier = "MyPageServiceCell"
+    
+    // MARK: - UI Components
+    
+    var serviceLable = UILabel()
+    var rightItemLabel = UILabel()
+    
+    // MARK: - init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
+        hieararchy()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Functions
+    
+    func configureUI() {
+        
+        serviceLable.do {
+            $0.font = .body3()
+        }
+        rightItemLabel.do {
+            $0.font = .body3()
+        }
+    
+    }
+    
+    private func hieararchy() {
+        contentView.addSubViews(serviceLable,
+                                rightItemLabel)
+    }
+    
+    private func setLayout() {
+        serviceLable.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(30)
+        }
+        rightItemLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(30)
+        }
+    }
+}
