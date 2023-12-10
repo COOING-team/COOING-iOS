3e950ac8cf43436c0bdde4fcc6e9f45f4d418115
CJiu01 <jiwoo020801@gmail.com>
2023-12-11 04:51:31 +0900

    feat/ MyPageModel #39

--- COOING/Presentation/MyPage/View/MyPageModel.swift
+++ COOING/Presentation/MyPage/View/MyPageModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct MyPageLocalData: AppData {
+    let titleLabel: String
+}
+
+extension MyPageLocalData {
+    static let myPageServiceLabelList = [MyPageLocalData(titleLabel: "개인 정보 약관"),
+                                         MyPageLocalData(titleLabel: "로그아웃"),
+                                         MyPageLocalData(titleLabel: "탈퇴")]
+}
+
