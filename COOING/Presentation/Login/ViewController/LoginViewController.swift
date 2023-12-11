edd70e0fb9ba4775451efaec58fadce44dbd34b4
CJiu01 <jiwoo020801@gmail.com>
2023-12-11 22:50:50 +0900

    feat/ add asset #43

--- COOING/Presentation/Login/ViewController/LoginViewController.swift
+++ COOING/Presentation/Login/ViewController/LoginViewController.swift
@@ -22,6 +22,7 @@ class LoginViewController: BaseViewController {
     
     private let loginCooingLogoImage = UIImageView(image: Image.loginCooingLogo)
     private let kakaoLoginButton = UIButton()
+    private let appleLoginButton = UIButton()
 
     // MARK: - override Functions
     
